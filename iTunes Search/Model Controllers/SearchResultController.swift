febf6d6040780340a1200fffd4ee832693b89940
Marlon <marmoneymar@icloud.com>
2019-09-16 12:25:14 -0400

    Added first test case

--- iTunes Search/Model Controllers/SearchResultController.swift
+++ iTunes Search/Model Controllers/SearchResultController.swift
@@ -40,7 +40,8 @@ class SearchResultController {
         }
         dataTask.resume()
     }
-    
+
+	// https://itunes.apple.com/search?entity=software&term=garageband
     let baseURL = URL(string: "https://itunes.apple.com/search")!
     var searchResults: [SearchResult] = []
 }
---
+++ iTunes SearchTests/SearchResultControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultControllerTests.swift
+//  iTunes SearchTests
+//
+//  Created by Marlon Raskin on 9/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import iTunes_Search
+
+
+class SearchResultsControllerTests: XCTestCase {
+
+
+	func testSearchResultController() {
+		let controller = SearchResultController()
+	}
+}
